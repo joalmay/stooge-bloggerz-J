1049c502684242e11255dd3886d7095decdeffdf
joalmay <joalmay@gmail.com>
2014-01-31 15:52:24 -0700

    Step3
    Signed-off-by: joalmay <joalmay@gmail.com>

---
+++ app/scripts/controllers/blog.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('stoogeBloggerzApp')
+  .controller('BlogCtrl', function ($scope) {
+  		$scope.blogPosts =    [
+		  { title: 'Moe says...', body: '"Why I oughta..."', author: 'Moe'},
+		  { title: 'Curly says...', body: '"Nyuk Nyuk Nyuk!"', author: 'Curly'},
+		  { title: 'Moe says...', body: '"You nitwit!"', author: 'Moe'},
+		  { title: 'Larry says...', body: '"What\'s the idea?"', author: 'Larry'}
+	];
+
+	var blog = {};  
+
+  });
